671d2d5623f9d0bfa56a3e76a02d82137ab1e426
Léa LEE <lealee@MacBook-Pro-de-Lea.local>
2022-08-02 12:55:26 +0200

    update v

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,19 +4,24 @@ import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
 
+// The value of ‘this’ in JavaScript functions is determined by how a function is called.
+// Methods like call, apply, and bind explicitly set the value of ‘this’ in a function,
+//  however if ‘this’ isn’t explicitly set, then ‘this’ will default to the global context.
+
 class App extends Component {
   constructor() {
-    super();
+    super(); // automatic, syntax
     this.state = {
       robots: [],
       searchfield: "",
     };
   }
-
+  //méthode de cycle de vie : pour appeler la méthode setState afin de changer l'état de l'application
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ robots: users }));
+    // setState = ppour changer un state The state of a component can change either due to a response to an action performed by the user or an event triggered by the system. Whenever the state changes, React re-renders the component to the browser. Before updating the value of the state, we need to build an initial state setup. Once we are done with it, we use the setState() method to change the state object. It ensures that the component has been updated and calls for re-rendering of the component.
   }
 
   onSearchChange = (event) => {
